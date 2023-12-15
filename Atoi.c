73d1453dfcd99600c0d1feba759a7e3dff8482e8
Eseoghene-ODODO <en.ododo@gmail.com>
2023-12-15 07:23:01 +0000

    Newly updated

--- Atoi.c
+++ Atoi.c
@@ -14,7 +14,7 @@ int interactive(info_t *info)
 /**
  * is_delim - checks if character is a delimeter
  * @c: the char to check
- * @*d: the delimeter string
+ * @d: the delimeter string
  * Return: 1 if true, 0 if false
  */
 int is_delim(char c, char *d)
